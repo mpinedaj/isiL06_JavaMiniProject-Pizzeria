502e8a429bdd74d64b969e3815ac760834536092
c.pineda <c.pineda@unillanos.edu.co>
2026-03-07 17:40:17 -0500

    Add pizza pricing constants to Reglas class

--- src/services/Reglas.java
+++ src/services/Reglas.java
@@ -2,5 +2,8 @@ package services;
 
 public class Reglas {
     public static final int MAX_CANT = 8;
+    public static final double PRECIO_PIZZA_PEQUENA = 18000;
+    public static final double PRECIO_PIZZA_MEDIANA = 28000;
+    public static final double PRECIO_PIZZA_GRANDE = 38000;
     public static final int CANT_DESCUENTO = 4;
 }
